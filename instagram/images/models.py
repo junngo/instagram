1513557f38ad651cf06ab77dc7177f2a3ef891ee
Myeongjun Go <myeongjun.ko@gmail.com>
2018-03-11 22:15:52 +0900

    creating the modesl and registering the models in the adming

--- instagram/images/models.py
+++ instagram/images/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from instagram.users import models as user_models
 
 # Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Image(TimeStampedModel):
+
+    """ Image Model """
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+
+class Comment(TimeStampedModel):
+
+    """ Comment Model """
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User,null=True, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
