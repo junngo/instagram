c62da66aa97d9d3b5b43770ec33fbb1ec615622c
Myeongjun Go <myeongjun.ko@gmail.com>
2018-03-16 22:46:54 +0900

    creating URLS, testing serializers, nested serializers

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
 
     # User management
     url(r'^users/', include('instagram.users.urls', namespace='users')),
+    url(r'^images/', include('instagram.images.urls', namespace='images')),
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
--- instagram/images/serializers.py
+++ instagram/images/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
+from . import models
 
-class ImageSerializer(serializers.Serializer):
+class ImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Image
         fields = '__all__'
 
-class CommentSerializer(serializers.Serializer):
+class CommentSerializer(serializers.ModelSerializer):
+
+    image = ImageSerializer()
 
     class Meta:
         model = models.Comment
         fields = '__all__'
 
-class LikeSerializer(serializers.Serializer):
+class LikeSerializer(serializers.ModelSerializer):
+
+    image = ImageSerializer()
 
     class Meta:
-        models = models.Like
+        model = models.Like
         fields = '__all__'
