c424748fbe4b0dec4e7485848acbd085225896af
Myeongjun Go <myeongjun.ko@gmail.com>
2018-03-17 17:18:32 +0900

    hidden model fiedlds, Set.

--- instagram/images/serializers.py
+++ instagram/images/serializers.py
@@ -1,15 +1,8 @@
 from rest_framework import serializers
 from . import models
 
-class ImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.Image
-        fields = '__all__'
 
 class CommentSerializer(serializers.ModelSerializer):
-
-    image = ImageSerializer()
 
     class Meta:
         model = models.Comment
@@ -17,8 +10,27 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class LikeSerializer(serializers.ModelSerializer):
 
-    image = ImageSerializer()
-
     class Meta:
         model = models.Like
         fields = '__all__'
+
+class ImageSerializer(serializers.ModelSerializer):
+
+    # default name
+    # comment_set = CommentSerializer(many=True)
+    # like_set = LikeSerializer(many=True)
+
+    comments = CommentSerializer(many=True)
+    likes = LikeSerializer(many=True)
+
+    class Meta:
+        model = models.Image
+        #fields = '__all__'
+        fields = (
+            'id',
+            'file',
+            'location',
+            'caption',
+            'comments',
+            'likes'
+        )
