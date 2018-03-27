dd356696dc6d18bef328fda9c47efa0cf6fbb5e5
Myeongjun Go <myeongjun.ko@gmail.com>
2018-03-28 00:01:41 +0900

    deleting my own comment

--- instagram/images/urls.py
+++ instagram/images/urls.py
@@ -11,13 +11,18 @@ urlpatterns = [
     ),
     url(
         #(?P<var_name>regx)
-        regex = r'(?P<image_id>[0-9]+)/like/',
+        regex = r'(?P<image_id>[0-9]+)/likes/$',
         view = views.LikeImage.as_view(),
         name='like_image'
     ),
     url(
-        regex = r'(?P<image_id>[0-9]+)/comment/',
+        regex = r'(?P<image_id>[0-9]+)/comments/$',
         view = views.CommentOnImage.as_view(),
         name='comment_image'
+    ),
+    url(
+        regex = r'comments/(?P<comment_id>[0-9]+)/$',
+        view = views.Comment.as_view(),
+        name = 'comment'
     )
 ]
