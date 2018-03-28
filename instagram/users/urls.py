a7e3522c9e44f24e96ff66cbdaeaf3f128d9bf38
Myeongjun Go <myeongjun.ko@gmail.com>
2018-03-28 22:07:33 +0900

    get people to follow on(explore)

--- instagram/users/urls.py
+++ instagram/users/urls.py
@@ -5,23 +5,8 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     url(
-        regex=r'^$',
-        view=views.UserListView.as_view(),
-        name='list'
-    ),
-    url(
-        regex=r'^~redirect/$',
-        view=views.UserRedirectView.as_view(),
-        name='redirect'
-    ),
-    url(
-        regex=r'^~update/$',
-        view=views.UserUpdateView.as_view(),
-        name='update'
-    ),
-    url(
-        regex=r'^(?P<username>[\w.@+-]+)/$',
-        view=views.UserDetailView.as_view(),
-        name='detail'
+        regex=r'^explore/$',
+        view=views.ExploreUsers.as_view(),
+        name='explore_users'
     ),
 ]
--- instagram/users/views.py
+++ instagram/users/views.py
@@ -1,44 +1,14 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse
-from django.views.generic import DetailView, ListView, RedirectView, UpdateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . import models, serializers
 
-from .models import User
+class ExploreUsers(APIView):
 
+    def get(self, request, format=None):
 
-class UserDetailView(LoginRequiredMixin, DetailView):
-    model = User
-    # These next two lines tell the view to index lookups by username
-    slug_field = 'username'
-    slug_url_kwarg = 'username'
+        last_five = models.User.objects.all().order_by('-date_joined')[:5]
 
+        serializer = serializers.ExploreUserSerializer(last_five, many=True)
 
-class UserRedirectView(LoginRequiredMixin, RedirectView):
-    permanent = False
-
-    def get_redirect_url(self):
-        return reverse('users:detail',
-                       kwargs={'username': self.request.user.username})
-
-
-class UserUpdateView(LoginRequiredMixin, UpdateView):
-
-    fields = ['name', ]
-
-    # we already imported User in the view code above, remember?
-    model = User
-
-    # send the user back to their own page after a successful update
-    def get_success_url(self):
-        return reverse('users:detail',
-                       kwargs={'username': self.request.user.username})
-
-    def get_object(self):
-        # Only get the User record for the user making the request
-        return User.objects.get(username=self.request.user.username)
-
-
-class UserListView(LoginRequiredMixin, ListView):
-    model = User
-    # These next two lines tell the view to index lookups by username
-    slug_field = 'username'
-    slug_url_kwarg = 'username'
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
