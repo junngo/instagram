3a93670b0500564f1c203995b5d75c2a489edd1e
Myeongjun Go <myeongjun.ko@gmail.com>
2018-03-31 22:29:12 +0900

    searching by hashtag

--- instagram/images/serializers.py
+++ instagram/images/serializers.py
@@ -3,7 +3,7 @@ from . import models
 from instagram.users import models as user_models
 
 
-class UserProfileImageSerializer(serializers.ModelSerializer):
+class CountImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Image
--- instagram/images/views.py
+++ instagram/images/views.py
@@ -124,3 +124,14 @@ class Search(APIView):
     def get(self, request, format=None):
 
         hashtags = request.query_params.get('hashtags', None)
+
+        if hashtags is not None:
+
+            hashtags = hashtags.split(",")
+            images = models.Image.objects.filter(tags__name__in=hashtags).distinct()
+            serializer = serializers.CountImageSerializer(images, many=True)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
