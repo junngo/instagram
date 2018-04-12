70846d925d12f7ffba2f979f476c5a9611e4ac1e
Myeongjun Go <myeongjun.ko@gmail.com>
2018-04-12 23:02:46 +0900

    updating the password

--- instagram/users/urls.py
+++ instagram/users/urls.py
@@ -38,5 +38,10 @@ urlpatterns = [
         regex = r'^(?P<username>\w+)/$',
         view = views.UserProfile.as_view(),
         name = 'user_profile'
+    ),
+    url(
+        regex = r'^(?P<username>\w+)/password/$',
+        view = views.ChangePassword.as_view(),
+        name = 'change'
     )
 ]
