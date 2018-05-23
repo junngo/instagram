3278abd63aa3d15f8755eccfcab77110a5b4d908
Myeongjun Go <myeongjun.ko@gmail.com>
2018-05-23 22:53:09 +0900

    controlling inputs and submit on loginform

--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -1,17 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Ionicon from "react-ionicons";
 import formStyles from "shared/formStyles.scss";
 
 const LoginForm = props => (
   <div className={formStyles.formComponent}>
-    <form className={formStyles.form}>
-      <input type="text" placeholder="Username" className={formStyles.textInput} />
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        className={formStyles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name="username"
+      />
       <input
         type="password"
         placeholder="Password"
         className={formStyles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
       />
-      <input type="submit" value="Log in" className={formStyles.button} />
+      <input
+        type="submit"
+        value="Log in"
+        className={formStyles.button}
+      />
     </form>
     <span className={formStyles.divider}>or</span>
     <span className={formStyles.facebookLink}>
@@ -21,5 +36,12 @@ const LoginForm = props => (
     <span className={formStyles.forgotLink}>Forgot password?</span>
   </div>
 );
+
+LoginForm.propTypes = {
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
 
 export default LoginForm;
