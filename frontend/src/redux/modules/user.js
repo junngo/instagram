aca4a8b54ab788b93d9251c42fe087289a03892b
Myeongjun Go <myeongjun.ko@gmail.com>
2018-05-29 22:59:22 +0900

    setting the token with facebook login

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -1,8 +1,15 @@
 // imports
 
 // actions
+const SAVE_TOKEN = "SAVE_TOKEN";
 
 // action creators
+function saveToken(token){
+  return {
+    type: SAVE_TOKEN,
+    token
+  }
+}
 
 // API actions
 function facebookLogin(access_token){
@@ -17,26 +24,41 @@ function facebookLogin(access_token){
       })
     })
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+      if(json.token){
+        localStorage.setItem("jwt", json.token);
+        dispatch(saveToken(json.token));
+      }
+    })
     .catch(err => console.log(err));
   };
 }
 
 // initial state
 const initialState = {
-  isLoggedIn: localStorage.getItem("jwt") || false
+  isLoggedIn: localStorage.getItem("jwt") ? true : false
 };
 
 
 // reducer
 function reducer(state = initialState, action){
   switch (action.type) {
+    case SAVE_TOKEN:
+      return applySetToken(state, action);
     default:
       return state;
   }
 }
 
 // reducer functions
+function applySetToken(state, action){
+  const { token } = action;
+  return {
+    ...state,
+    isLoggedIn: true,
+    token
+  }
+}
 
 // exports
 const actionCreators = {
