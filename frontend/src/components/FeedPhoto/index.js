890bda313d27098833f25a06b389441e8aeb8bd9
Myeongjun Go <myeongjun.ko@gmail.com>
2018-06-18 20:42:39 +0900

    getting and setting the likes

--- frontend/src/components/FeedPhoto/index.js
+++ frontend/src/components/FeedPhoto/index.js
@@ -1,4 +1,13 @@
 import { connect } from "react-redux";
 import Container from "./container";
+import { actionCreators as UserActions } from "redux/modules/user";
 
-export default connect()(Container);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    getPhotoLikes: () => {
+      dispatch(UserActions.getPhotoLikes(ownProps.id));
+    }
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Container);
